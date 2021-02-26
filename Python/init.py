a7605577cdd1bd478cb20b1313ecef6041cec4cd
shaoxc <shaoxcnmg@gmail.com>
2021-02-26 15:07:08 -0500

    add function to clean saved _arrays

--- Python/init.py
+++ Python/init.py
@@ -5,3 +5,15 @@
 
 
 # atexit.register(pwscf_finalise)
+
+import pkgutil
+import operator
+def pwpy_clean_saved():
+    mods = [name for _, name, _ in pkgutil.iter_modules(pwscfpy.__path__)]
+    for mod in mods :
+        if hasattr(pwscfpy, mod):
+            mod += '._arrays'
+            operator.attrgetter(mod)(pwscfpy).clear()
+
+
+pwpy_clean_saved()
