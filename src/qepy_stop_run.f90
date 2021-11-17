a1623f7da810b89024c2ee8070c5bc8f81427876
shaoxc <shaoxcnmg@gmail.com>
2021-11-17 11:22:10 -0500

    fix: free all comms

--- src/qepy_stop_run.f90
+++ src/qepy_stop_run.f90
@@ -55,7 +55,7 @@
   !!
 !END SUBROUTINE stop_run
 
-SUBROUTINE qepy_stop_run( exit_status, print_flag, what )
+SUBROUTINE qepy_stop_run( exit_status, print_flag, what, finalize )
   !----------------------------------------------------------------------------
   !! Close all files and synchronize processes before stopping:
   !
@@ -65,21 +65,45 @@ SUBROUTINE qepy_stop_run( exit_status, print_flag, what )
   !
   !! Do not remove temporary files needed for restart.
   !
+  !qepy --> 
+  ! Also add some from pwscf and run_pwscf
+  ! Merge and modify the mp_global.mp_global_end
+  !qepy <-- 
+  !
   USE io_global,          ONLY : stdout, ionode
   USE mp_global,          ONLY : mp_global_end
   USE environment,        ONLY : environment_end
   USE io_files,           ONLY : iuntmp, seqopn
+  USE qmmm,               ONLY : qmmm_shutdown
+  USE qexsd_module,       ONLY : qexsd_set_status
+  USE mp,                 ONLY : mp_comm_free, mp_barrier, mp_start, mp_end, mp_stop, mp_count_nodes
+  USE mp_world,           ONLY : world_comm
+  USE mp_bands,           ONLY : inter_bgrp_comm, intra_bgrp_comm
+  USE mp_pools,           ONLY : inter_pool_comm, intra_pool_comm
+  USE mp_exx,             ONLY : inter_egrp_comm, intra_egrp_comm
+  USE mp_images,          ONLY : inter_image_comm, intra_image_comm
+  USE mp_orthopools,      ONLY : mp_stop_orthopools
+  USE mp_diag,            ONLY : ortho_comm
   !
   IMPLICIT NONE
   !
   INTEGER, INTENT(IN) :: exit_status
   INTEGER, INTENT(IN), OPTIONAL :: print_flag
   CHARACTER(LEN=*), INTENT(IN), OPTIONAL :: what
+  LOGICAL, OPTIONAL   :: finalize
   CHARACTER(LEN=256)  :: what_ ='config-nowf'
   INTEGER             :: iprint
   LOGICAL             :: exst, opnd, lflag
+#if defined(__MPI)
+  INTEGER :: ierr
+#endif
   !
-
+  !qepy --> pwscf and run_pwscf
+  CALL qexsd_set_status( exit_status )
+  CALL qmmm_shutdown()
+  if ( ortho_comm  /= 0 .and. ortho_comm  /= world_comm ) CALL laxlib_free_ortho_group()
+  !qepy <-- pwscf and run_pwscf
+  !
   IF ( PRESENT(what) .and. len_trim(what)>1) THEN
      what_= trim(what)
   ENDIF
@@ -126,7 +150,28 @@ SUBROUTINE qepy_stop_run( exit_status, print_flag, what )
      CALL environment_end( 'PWSCF' )
   ENDIF
   !
-  CALL mp_global_end()
+  !CALL mp_global_end()
+  !-----------------------------------------------------------------------
+  !qepy --> add mp_global_end
+  if ( intra_egrp_comm  /= 0 .and. intra_egrp_comm  /= world_comm ) CALL mp_comm_free ( intra_egrp_comm )
+  if ( inter_egrp_comm  /= 0 .and. inter_egrp_comm  /= world_comm ) CALL mp_comm_free ( inter_egrp_comm )
+  if ( intra_bgrp_comm  /= 0 .and. intra_bgrp_comm  /= world_comm ) CALL mp_comm_free ( intra_bgrp_comm )
+  if ( inter_bgrp_comm  /= 0 .and. inter_bgrp_comm  /= world_comm ) CALL mp_comm_free ( inter_bgrp_comm )
+  if ( intra_pool_comm  /= 0 .and. intra_pool_comm  /= world_comm ) CALL mp_comm_free ( intra_pool_comm )
+  if ( inter_pool_comm  /= 0 .and. inter_pool_comm  /= world_comm ) CALL mp_comm_free ( inter_pool_comm )
+  if ( intra_image_comm /= 0 .and. intra_image_comm /= world_comm ) CALL mp_comm_free ( intra_image_comm )
+  if ( inter_image_comm /= 0 .and. inter_image_comm /= world_comm ) CALL mp_comm_free ( inter_image_comm )
+  CALL mp_stop_orthopools( ) ! cleans orthopools if used in exx
+  CALL mp_barrier( world_comm )
+  CALL mp_end ( world_comm )
+#if defined(__MPI)
+  IF (present(finalize) .and. finalize) THEN
+     CALL mpi_finalize(ierr)
+     IF (ierr/=0) CALL mp_stop( 8002 )
+  END IF
+#endif
+  !qepy <-- add mp_global_end
+  !-----------------------------------------------------------------------
   !
 END SUBROUTINE qepy_stop_run
 !
