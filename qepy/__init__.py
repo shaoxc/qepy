7d7a0ac2dd99416b1ee12246581540d771b45e6b
shaoxc <shaoxcnmg@gmail.com>
2023-02-14 14:28:44 -0500

    update version

--- qepy/__init__.py
+++ qepy/__init__.py
@@ -19,9 +19,9 @@ def qepy_clean_saved():
 qepy_clean_saved()
 __author__ = "Pavanello Research Group"
 __contact__ = "m.pavanello@rutgers.edu"
-__version__ = "0.0.1rc2"
+__version__ = "0.0.1"
 __license__ = "GPL"
-__date__ = "2022-10-21"
+__date__ = "2023-02-14"
 
 try:
     from importlib.metadata import version # python >= 3.8
