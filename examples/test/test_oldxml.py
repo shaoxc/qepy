f4620457ac96977321650a9dfda34588ae323745
shaoxc <shaoxcnmg@gmail.com>
2023-03-28 14:34:57 -0400

    clean one test file

--- examples/test/test_oldxml.py
+++ examples/test/test_oldxml.py
@@ -20,6 +20,8 @@ def test_oldxml_collect():
     driver = Driver(comm = comm, prefix = 'al_oldxml', outdir=path, task = 'nscf')
     wfc = path / 'al_oldxml.wfc1'
     if wfc.is_file(): wfc.unlink()
+    wfc = path / 'al_oldxml.wfc'
+    if wfc.is_file(): wfc.unlink()
     energy = driver.get_energy()
     assert np.isclose(energy, -552.93477389, atol = 1E-6)
     driver.stop(what = 'no')
