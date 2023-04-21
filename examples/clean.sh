11269147daf8107e01cd822c8dcaca1db03c3317
shaoxc <shaoxcnmg@gmail.com>
2023-04-21 16:54:27 -0400

    small update

--- examples/clean.sh
+++ examples/clean.sh
@@ -1,3 +1,3 @@
 find ./ -name 'tmp.*' -exec rm -r {} \;
 find ./ -name 'qepy_input_tmp.in' -exec rm  {} \;
-find ./jupyter/ -name 'pwscf.*' -exec rm  {} \;
+find ./jupyter/ -name 'pwscf.*' -exec rm -r {} \;
