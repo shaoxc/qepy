beb69bb4daa72bada301b92e7194b1c04515c8a4
shaoxc <shaoxcnmg@gmail.com>
2024-05-03 14:39:56 -0400

    add some custom functions

--- qepy/cui/main.py
+++ qepy/cui/main.py
@@ -1,21 +1,52 @@
 #!/usr/bin/env python3
 import argparse
+from importlib import import_module
 from qepy.cui import qex
 
-def get_args():
-    parser = argparse.ArgumentParser(description='QEpy tasks :')
+QEPYBINS = {
+    'pw2pp.py' : 'qepy.cui.pw2pp',
+}
+
+def get_parse():
+    def formatter(prog):
+        return argparse.HelpFormatter(prog, max_help_position=30, width=100)
+    parser = argparse.ArgumentParser(description='QEpy tasks :', formatter_class=formatter)
     #-----------------------------------------------------------------------
     for prog in qex.QEBINS :
-        parser = qex.get_parse(prog=prog, parser=parser)
+        p = qex.get_parse(prog=prog)
+        parser.add_argument(f'--{prog}', dest=prog, action='store_true',
+                default=False, help = p.description)
+    for prog, modfile in QEPYBINS.items():
+        mod = import_module(modfile)
+        p = mod.get_parse()
+        parser.add_argument(f'--{prog}', dest=prog, action='store_true',
+                default=False, help = p.description)
     #-----------------------------------------------------------------------
-    args = parser.parse_args()
-    return args
+    return parser
+
+def get_job_name(argv):
+    engine = key = None
+    for arg in argv[1:] :
+        if arg.startswith('--') :
+            key = arg[2:]
+            if key in qex.QEBINS:
+                engine = 'qe'
+                break
+            elif key in QEPYBINS:
+                engine = 'qepy'
+                break
+    if engine: argv.remove('--'+key)
+    return engine, key
+
 
 def main():
     import sys
-    for prog in qex.QEBINS :
-        key = '--' + prog
-        if key in sys.argv :
-            return qex.main(prog)
-    else :
-        get_args()
+    engine, prog = get_job_name(sys.argv)
+    if engine == 'qe' :
+        return qex.main(prog)
+    elif engine == 'qepy' :
+        mod = import_module(QEPYBINS[prog])
+        return mod.main()
+    else:
+        parser = get_parse()
+        parser.print_help()
