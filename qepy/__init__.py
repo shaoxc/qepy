8153127d63ae11948f75732276df3cb5feff1fa6
shaoxc <shaoxcnmg@gmail.com>
2024-05-23 23:03:11 -0400

    improve the installation

--- qepy/__init__.py
+++ qepy/__init__.py
@@ -1,31 +1,21 @@
 import sys
-import os
 from pathlib import Path
-from ctypes import util, CDLL, RTLD_LOCAL, RTLD_GLOBAL
 from importlib import import_module
 from importlib.metadata import version # python >= 3.8
+from .__config__ import CONFIG, show as show_config
+from .core import QEpyMods, find_library, load_library
+
+__all__ = ["show_config"]
+
+# fix MPI_IN_PLACE
+mpilib = find_library('mpifort') or find_library('mpi')
+if mpilib : load_library(mpilib)
+# fix mkl only if mkl library was linked
+if 'mkl_' in CONFIG.get('QE', {}).get('link', ''): load_library('mkl_rt')
+
 path = Path(__file__).resolve().parent/'qepylibs'
 sys.path.insert(0, str(path))
 __path__.append(str(path))
-# FIX MPI_IN_PLACE and MKL
-if hasattr(util, '_findLib_ld') and hasattr(util, '_get_soname') :
-    mpilib = util._get_soname(util._findLib_ld('mpi'))
-else :
-    mpilib = None
-mpilib = util.find_library('mpifort') or mpilib or util.find_library('mpi')
-try:
-    CDLL(mpilib, RTLD_LOCAL | RTLD_GLOBAL)
-except Exception :
-    pass
-try:
-    if hasattr(util, '_findLib_ld'):
-        mkllib = os.path.basename(util._findLib_ld('mkl_rt'))
-    else :
-        mkllib = util.find_library('mkl_rt')
-    CDLL(mkllib, RTLD_LOCAL | RTLD_GLOBAL)
-except Exception :
-    pass
-# End Fix
 
 __author__ = "Pavanello Research Group"
 __contact__ = "m.pavanello@rutgers.edu"
@@ -39,7 +29,6 @@ except Exception :
     pass
 
 # compatible with older versions API
-from .core import QEpyMods
 constants = QEpyMods('qepy_modules.constants')
 qepy_common = QEpyMods('qepy_pw.qepy_common')
 
